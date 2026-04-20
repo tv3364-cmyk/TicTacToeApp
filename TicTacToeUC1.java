8b9b22ec3113037604608aa7a180612ab26e9402
Your Name <your_uni_or_personal_email>
2026-04-20 08:59:52 +0530

    UC1: Display empty Tic-Tac-Toe board

---
+++ TicTacToeUC1.java
@@ -0,0 +1,22 @@
+public class TicTacToeUC1 {
+
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+
+        // Initialize board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
